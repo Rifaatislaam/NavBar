efe7030df3925eb8215a9b5b4700f7efe9b25162
Rifaatislaam <mdrifaatislaam@gmail.com>
2026-05-02 23:49:01 +0600

    remove js file

--- script.js
+++
@@ -1,9 +0,0 @@
-let x = document.getElementById(`menu`);
-let menuIcon = document.getElementById(`menuIcon`);
-let closeIcon = document.getElementById(`closeIcon`);
-
-function aa() {
-  x.style.color = `red`;
-}
-
-
